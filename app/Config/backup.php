5665d185b4c0bed48aef5f85e2522555a22f7395
maartenderie <maartenderie@gmail.com>
2026-06-18 12:09:08 +0200

    change backup name to accomodate backup monitor

--- app/Config/backup.php
+++ app/Config/backup.php
@@ -7,7 +7,10 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'laravel-backup'),
+
+        // ! ! ! backup-monitor does not support spaces ! ! !
+
+        'name' => 'Sundata-Wiki',
 
         'source' => [
             'files' => [
